a6719391d274624e21ca6a8bfabdbf5d8b5cb1f1
richard sandblom <r.sandblom@gmail.com>
2020-11-18 18:36:32 +0100

    Add project files.

---
+++ JenkinsBuildTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace JenkinsBuildTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = AddTwoNumbers(1, 2);
+
+            Console.WriteLine(i);
+        }
+
+        private static int AddTwoNumbers(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
